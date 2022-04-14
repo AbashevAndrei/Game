a3807032d0627384f7bcd9c28f0e1d7d522b2cc9
Abashev Andrew <abashev77@gmail.com>
2022-04-14 22:36:41 +1000

    First round complete

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
-let question = prompt('Камни, ножницы, бумага', '')
+let arr = ['Камень', 'Ножницы', 'Бумага']
+let arr1 = []
 
-alert(`Ты выбрал ${question}!`)
+let playerSelection = prompt('Камень, ножницы или бумага?', '')
 
+// Выводит сообщение, пока пользователь что-нибудь не напечатает (вопрос повторяется и при нажатии Esc)
+while (playerSelection == '' || playerSelection == null) {
+  playerSelection = prompt('Камень, ножницы или бумага?', '')
+}
+
+alert(`Ты выбрал: ${playerSelection}!`)
+
+// Случайный выбор компьютера
+function computerPlay(arr) {
+  let rand = Math.floor(Math.random() * arr.length)
+  return arr[rand]
+}
+
+let computerSelection = computerPlay(arr)
+
+alert(`Компьютер выбрал: ${computerSelection}`)
+
+let victory = 'Вы победили!'
+let lose = 'Вы проиграли!'
+let draw = 'Ничья, попробуйте снова'
+
+// Раунд 1
+function round(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return alert('Ничья, попробуйте снова')
+  }
+  if (playerSelection == 'Камень' && computerSelection == 'Ножницы') {
+    return alert(victory)
+  }
+  if (playerSelection == 'Камень' && computerSelection == 'Бумага') {
+    return alert(lose)
+  }
+  if (playerSelection == 'Бумага' && computerSelection == 'Камень') {
+    return alert(victory)
+  }
+  if (playerSelection == 'Бумага' && computerSelection == 'Ножницы') {
+    return alert(lose)
+  }
+  if (playerSelection == 'Ножницы' && computerSelection == 'Бумага') {
+    return alert(victory)
+  }
+  if (playerSelection == 'Ножницы' && computerSelection == 'Камень') {
+    return alert(lose)
+  }
+}
+
+round(playerSelection, computerSelection)
